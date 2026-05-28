5a972c3f0475a816fd43619342b4a9bde1ec6926
Victor <vrush0998@gmail.com>
2026-05-28 10:42:38 -0700

    added new code

--- src/main/java/com/myFinch/FinalFinch.java
+++ src/main/java/com/myFinch/FinalFinch.java
@@ -8,8 +8,16 @@ public class FinalFinch extends Finch {
         super(device);
     }
 
-    public void navigateMaze() {
-        bird.setMove(direction: "F", distance: 10, speed: 50);
+    public void doubleLeftTurn() {
+        setTurn("L", 90, 100);
+        setMove("F", 20, 50);
+        setTurn("L", 90, 100);
+    }
+
+    public void doubleRightTurn() {
+        setTurn("R", 90, 100);
+        setMove("F", 20, 50);
+        setTurn("R", 90, 100);
     }
 
 }
--- src/main/java/com/myFinch/TesterFinch.java
+++ src/main/java/com/myFinch/TesterFinch.java
@@ -3,9 +3,18 @@ package com.myFinch;
 public class TesterFinch {
     public static void main(String[] args) {
         
-        FinalFinch bird = new FinalFinch(device: "A");
+        FinalFinch bird = new FinalFinch("A");
         
-        bird.navigateMaze();
+        bird.setMove("F", 90, 100);
+        bird.doubleLeftTurn();
+        bird.setMove("F", 90, 100);
+        bird.doubleRightTurn();
+        bird.setMove("F", 90, 100);
+        bird.doubleLeftTurn();
+        bird.setMove("F", 90, 100);
+        bird.doubleRightTurn();
+        bird.setMove("F", 120, 100);
+        
         
     }
 }
