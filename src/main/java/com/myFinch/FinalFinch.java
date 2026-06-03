4a384a25e5c38b71365a578e49a75184e9c1b2e5
Victor <vrush0998@gmail.com>
2026-06-03 10:40:55 -0700

    Final Commit

--- src/main/java/com/myFinch/FinalFinch.java
+++ src/main/java/com/myFinch/FinalFinch.java
@@ -33,4 +33,40 @@ public class FinalFinch extends Finch {
         setTurn("R", COMPLETE_TURN, FULL_SPEED);
     }
 
+    public void beginningSection() {
+        setMove("F", 60, 100);
+        doubleLeftTurn();
+        setMove("F", 60, 100);
+        doubleRightTurn();
+        setMove("F", 60, 100);
+        doubleLeftTurn();
+        setMove("F", 60, 100);
+        doubleRightTurn();
+    }
+
+    public void middleSection() {
+        longMoveRight();
+        setMove("F", 100, 100);
+        doubleLeftTurn();
+        setMove("F", 100, 100);
+        setTurn("R", 90, 100);
+    }
+
+    public void endSection() {
+        longMoveRight();
+        longMoveRight();
+        setMove("F", 80, 100);
+        setTurn("R", 90, 100);
+        shortMoveRight();
+        shortMoveRight();
+        doubleRightTurn();
+        setMove("F", 30, 100);
+    }
+
+    public void navigateMaze() {
+        beginningSection();
+        middleSection();
+        endSection();
+    }
+
 }
